aebd8aba8f35c27ed58a662b342249f72a85128b
Gamal Ahmed <gamalahmed3265@gmail.com>
2023-07-26 09:10:23 +0300

    Type Annotations With Multidimensional Arrays

---
+++ Type Annotations With Multidimensional Arrays/dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+let ename = "famal";
+let age = 35;
+let hire = false;
+let tename = "famal";
+let tage = 35;
+let thire = false;
+let all = 343;
+let alle = "known";
+alle = "a";
+alle = 344;
+all = true;
+let myfiends = ["asfsf", "dsaf"];
+for (let index = 0; index < myfiends.length; index++) {
+    const element = myfiends[index];
+    console.log(element);
+}
+//# sourceMappingURL=index.js.map
---
+++ Type Annotations With Multidimensional Arrays/src/index.ts
@@ -0,0 +1,26 @@
+let ename="famal";
+let age=35;
+let hire=false;
+
+let tename:string="famal";
+let tage: number =35;
+let thire:boolean=false;
+let all:any=343;
+
+
+// Type Annotations With Multidimensional Arrays
+
+let alle:string| number|boolean="known";
+
+alle="a"
+alle=344;
+all=true;
+
+let myfiends:string[]=["asfsf","dsaf"]
+
+for (let index = 0; index < myfiends.length; index++) {
+    const element = myfiends[index];
+    console.log(element);
+    
+    
+}
