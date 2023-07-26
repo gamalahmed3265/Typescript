6696cea4ef113160900e68d73dab6f8b8dcb6616
Gamal Ahmed <gamalahmed3265@gmail.com>
2023-07-26 17:19:02 +0300

    Type Annotations With Object

---
+++ Type Annotations With Object/src/index.ts
@@ -0,0 +1,17 @@
+let myObject:{
+    readonly username:string,
+    id:number,
+    hire?:boolean,
+    skills:{
+        one:string,
+        two:string
+    }
+}={
+    username:"gamal",
+    id:203,
+    hire:false,
+    skills:{
+        one:"HTML",
+        two:"CSS"
+    }
+}
