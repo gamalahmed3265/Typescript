a8af1cc1c13a8638330949d022f40bd307e2f9c7
Gamal Ahmed <gamalahmed3265@gmail.com>
2023-07-26 09:04:07 +0300

    Learn TypescriptType Annotations With Arrays

---
+++ Learn TypescriptType Annotations With Arrays/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+let ename = "famal";
+let age = 35;
+let hire = false;
+let tename = "famal";
+let tage = 35;
+let thire = false;
+let all = 343;
+//# sourceMappingURL=index.js.map
---
+++ Learn TypescriptType Annotations With Arrays/src/index.ts
@@ -0,0 +1,8 @@
+let ename="famal";
+let age=35;
+let hire=false;
+
+let tename:string="famal";
+let tage: number =35;
+let thire:boolean=false;
+let all:any=343;
