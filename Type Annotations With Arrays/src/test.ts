808e22da55f23c2ebf36299abac09f43bbbf0def
Gamal Ahmed <gamalahmed3265@gmail.com>
2023-07-26 09:11:57 +0300

    Type Annotations With Arrays

---
+++ Type Annotations With Arrays/src/test.ts
@@ -0,0 +1,12 @@
+function add(n1:number,n2:number) {
+    return n1+n2;
+}
+let a=add(4,21);
+console.log(a);
+console.log(typeof(a));
+
+console.log("*".repeat(40));
+
+// let d=add(4,"21");
+// console.log(d);
+// console.log(typeof(d));
