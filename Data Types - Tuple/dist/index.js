4b2336d02502ef3b6bcbdac3d224efd5ad5225ac
Gamal Ahmed <gamalahmed3265@gmail.com>
2023-07-26 12:46:50 +0300

    Data Types - Tuple

---
+++ Data Types - Tuple/dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+let article = [100, "article", false];
+article = [34, "ar", true];
+article[1].replace("a", "e");
+let article2 = [100, "article", false];
+article2 = [34, "ar", true];
+article2[1].replace("a", "e");
+let [id, title, publshied] = article2;
+console.log(id);
+console.log(title);
+console.log(publshied);
+//# sourceMappingURL=index.js.map
---
+++ Data Types - Tuple/src/index.ts
@@ -0,0 +1,17 @@
+let article: [number,string,boolean] =[100,"article",false];
+
+article=[34,"ar",true];
+
+article[1].replace("a","e")
+
+let article2: readonly [number,string,boolean] =[100,"article",false];
+
+article2=[34,"ar",true];
+
+article2[1].replace("a","e")
+
+let [id,title,publshied]=article2
+
+console.log(id);
+console.log(title);
+console.log(publshied);
