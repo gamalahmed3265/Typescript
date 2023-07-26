9e4fa3daba7ac14cce2944ee0fdf4e322ee08b41
Gamal Ahmed <gamalahmed3265@gmail.com>
2023-07-26 19:16:24 +0300

    Generics Multiple Types

--- Generics Multiple Types/dist/index.js
+++ Generics Multiple Types/dist/index.js
@@ -1,8 +1,14 @@
 "use strict";
-function returnType(val) {
+function returnType(val, s) {
     return val;
 }
-console.log(returnType(100));
-console.log(returnType("gamal"));
-console.log(returnType(false));
+console.log(returnType(100, 33));
+console.log(returnType("gamal", "dsaf"));
+console.log(returnType(false, true));
+function returnType2(val, s) {
+    return val;
+}
+console.log(returnType2(100, 33));
+console.log(returnType2("gamal", "dsaf"));
+console.log(returnType2(false, true));
 //# sourceMappingURL=index.js.map
