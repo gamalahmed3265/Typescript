208fd67b53f49ffda20b14d32cacccce3ca639ae
Gamal Ahmed <gamalahmed3265@gmail.com>
2023-07-26 10:40:21 +0300

    Data Types - Type Alias

---
+++ Data Types - Type Alias/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let namee = "gamal";
+let alle = "gamal";
+alle = 890;
+console.log(alle);
+//# sourceMappingURL=index.js.map
---
+++ Data Types - Type Alias/src/index.ts
@@ -0,0 +1,11 @@
+type str=string;
+type num=number;
+
+let namee:str="gamal";
+
+type strnum=str|num;
+
+let alle:strnum="gamal";
+alle=890;
+
+console.log(alle);
